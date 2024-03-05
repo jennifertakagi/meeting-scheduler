36f3497d5f4284f7b060b3c09320275365aa2b89
Jennifer Takagi <jennifer.takagi@personio.de>
2024-03-05 11:05:42 +0100

    [feat] Add Home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Uncomplicated scheduling
+        </Heading>
+        <Text size="xl">
+          Connect your calendar and let people book appointments in your free
+          time.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
