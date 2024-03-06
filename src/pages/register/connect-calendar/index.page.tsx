289ff908cd2a107708cfca2f0d527e2892d54bb7
Jennifer Takagi <jennifer.takagi@personio.de>
2024-03-06 13:52:51 +0100

    [feat] Add error message when connecting to google calendar

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,21 +1,26 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
-import { ArrowRight } from 'phosphor-react'
+import { ArrowRight, Check } from 'phosphor-react'
 // import { api } from "../../../lib/axios"
 import { Container, Header } from '../styles'
-import { ConnectBox, ConnectItem } from './syles'
+import { AuthError, ConnectBox, ConnectItem } from './syles'
 import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 export default function Register() {
   const session = useSession()
+  const router = useRouter()
 
-  // async function handleRegister() {
+  const hasAuthError = !!router.query.error
+  const isSignedId = session.status === 'authenticated'
 
-  // }
+  async function handleConnectCalendar() {
+    await signIn('google')
+  }
 
   return (
     <Container>
       <Header>
-        <Heading as="strong">Connect your agenda!</Heading>
+        <Heading as="strong">Connect with Google Calendar!</Heading>
         <Text>
           Connect your calendar to automatically check the time busy schedules
           and new events as they are scheduled.
@@ -27,17 +32,31 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button
-            variant="secondary"
-            size="sm"
-            onClick={() => signIn('google')}
-          >
-            Connect
-            <ArrowRight />
-          </Button>
+          {isSignedId ? (
+            <Button size="sm" disabled>
+              Connected
+              <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Connect
+              <ArrowRight />
+            </Button>
+          )}
         </ConnectItem>
 
-        <Button type="submit">
+        {hasAuthError && (
+          <AuthError size="sm">
+            Failed to connect to Google, make sure you have enabled the Google
+            Calendar access permissions
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!isSignedId}>
           Next
           <ArrowRight />
         </Button>
