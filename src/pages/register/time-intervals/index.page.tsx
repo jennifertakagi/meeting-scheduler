0d60207b0f0cd8652a1d6f7704eebeabb0285565
Jennifer Takagi <jennifer.takagi@personio.de>
2024-03-07 12:19:38 +0100

    [feat] Add Checbox behaviour

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,7 +7,7 @@ import {
   TextInput,
 } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { useFieldArray, useForm } from 'react-hook-form'
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Container, Header } from '../styles'
 
@@ -28,21 +28,24 @@ export default function TimeIntervals() {
     handleSubmit,
     control,
     formState: { isSubmitting, errors },
+    watch,
   } = useForm({
     defaultValues: {
       intervals: [
-        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 0, enabled: true, startTime: '08:00', endTime: '18:00' },
         { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
         { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
         { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
         { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
-        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: false, startTime: '08:00', endTime: '18:00' },
         { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
       ],
     },
   })
 
   const weekDays = getWeekDays()
+
+  const intervals = watch('intervals')
 
   const { fields } = useFieldArray({
     control,
@@ -68,12 +71,26 @@ export default function TimeIntervals() {
             return (
               <IntervalItem key={field.id}>
                 <IntervalDay>
-                  <Checkbox />
+                  <Controller
+                    name={`intervals.${index}.enabled`}
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Checkbox
+                          onCheckedChange={(checked) =>
+                            field.onChange(checked === true)
+                          }
+                          checked={field.value}
+                        />
+                      )
+                    }}
+                  />
                   <Text>{weekDays[field.weekDay]}</Text>
                 </IntervalDay>
                 <IntervalInputs>
                   <TextInput
                     crossOrigin=""
+                    disabled={!intervals[index].enabled}
                     size="sm"
                     type="time"
                     step={60}
@@ -81,6 +98,7 @@ export default function TimeIntervals() {
                   />
                   <TextInput
                     crossOrigin=""
+                    disabled={!intervals[index].enabled}
                     size="sm"
                     type="time"
                     step={60}
--- src/utils/handlers/get-week-days.ts
+++ src/utils/handlers/get-week-days.ts
@@ -1,5 +1,5 @@
 export function getWeekDays() {
-  const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
+  const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
