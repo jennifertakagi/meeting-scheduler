03ee32cf0a4a8e352d1be1ca15e62b00b28b182f
Jennifer Takagi <jennifer.takagi@personio.de>
2024-03-13 21:28:29 +0100

    [feat] Load availability

--- src/pages/api/[username]/availability.api.ts
+++ src/pages/api/[username]/availability.api.ts
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../../lib/prisma'
+import { prisma } from '@/lib/prisma'
 
 export default async function handler(
   req: NextApiRequest,
@@ -56,5 +56,24 @@ export default async function handler(
     },
   )
 
-  return res.json({ possibleTimes })
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
+    )
+  })
+
+  return res.json({ possibleTimes, availableTimes })
 }
